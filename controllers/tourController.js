4867f4c5161c17e353a07d4145f5070c62b166ce
hemanth <hemanthceps@gmail.com>
2024-03-29 15:33:07 +0530

    second commit

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,19 +4,6 @@ import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
-
-const checkID = (req,res,next,val)=>{ 
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-  next();
-}
 
 const checkBody = (req,res,next) =>{
   if(!req.body.name || !req.body.price){
@@ -30,64 +17,64 @@ const checkBody = (req,res,next) =>{
 
 
 const getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    requestedAt: req.requestTime,
-    results: tours.length,
-    data: {
-      tours,
-    },
-  });
+  // res.status(200).json({
+  //   status: 'success',
+  //   requestedAt: req.requestTime,
+  //   results: tours.length,
+  //   data: {
+  //     tours,
+  //   },
+  // });
 };
 
 const getSpecificTour = (req, res) => {
   const id = req.params.id * 1; // converting the req.params(a string) to a number by multiplying it with 1
 
-  const tour = tours.find((el) => el.id === id);
+  // const tour = tours.find((el) => el.id === id);
 
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
+  // if (!tour) {
+  //   return res.status(404).json({
+  //     status: 'fail',
+  //     message: 'Invalid ID',
+  //   });
+  // }
 
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour,
-    },
-  });
+  // res.status(200).json({
+  //   status: 'success',
+  //   data: {
+  //     tour,
+  //   },
+  // });
 };
 
 const createTour = (req, res) => {
   const newId = tours.length;
   const newTour = Object.assign({ id: newId }, req.body);
 
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/../dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      if (err) throw err;
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
+  // tours.push(newTour);
+  // fs.writeFile(
+  //   `${__dirname}/../dev-data/data/tours-simple.json`,
+  //   JSON.stringify(tours),
+  //   (err) => {
+  //     if (err) throw err;
+  //     res.status(201).json({
+  //       status: 'success',
+  //       data: {
+  //         tour: newTour,
+  //       },
+  //     });
+  //   }
+  // );
 };
 
 const updateTour = (req, res) => {
 
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updated tour here...>',
-    },
-  });
+  // res.status(200).json({
+  //   status: 'success',
+  //   data: {
+  //     tour: '<Updated tour here...>',
+  //   },
+  // });
 };
 
 const deleteTour = (req, res) => {
@@ -98,4 +85,4 @@ const deleteTour = (req, res) => {
   });
 };
 
-export {getAllTours, getSpecificTour , createTour , updateTour, deleteTour, checkID , checkBody}
+export {getAllTours, getSpecificTour , createTour , updateTour, deleteTour , checkBody}
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,14 +1,14 @@
 import express from 'express';
-import {createTour , deleteTour , getAllTours , getSpecificTour , updateTour, checkID, checkBody} from './../controllers/tourController.js';
+import {createTour , deleteTour , getAllTours , getSpecificTour , updateTour, checkBody} from './../controllers/tourController.js';
 
 const router = express.Router();
 
-router.param('id', checkID)
+// router.param('id', checkID)
 
 router
   .route('/')
   .get(getAllTours)
-  .post(checkBody ,createTour);
+  .post(checkBody, createTour);
 
 router
   .route('/:id')
